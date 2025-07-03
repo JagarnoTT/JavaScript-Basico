f7229d6bd732e6a79d48e7e8d7ec37ed8730451c
Ericles Roque <jagarnotprev@gmail.com>
2025-07-03 20:03:10 -0300

    alteração

--- Listas&Objetos.js
+++ Listas&Objetos.js
@@ -1,9 +1,14 @@
 const names = ['Ericles','Bruna','Monica','Débora'];
 
 const João = names[1]; // Delcarando a possição que a variavel deve valer
-console.log(João)
-names.push('Diego');// Adicionando um elemento ao final da lista de objetos
+
+names.push('Diego', 'Roberto') // Adicionando elementos no final da lista
+
+names.pop();
+
 names.unshift('Andreia');// Adicionando um elemento ao inicio da lista
 console.log(names);
+console.log(João);
+
 
 
