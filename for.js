ca2206c5ddeea957b3d03bd93fd4912fae890d70
Ericles Roque <jagarnotprev@gmail.com>
2025-07-04 20:57:52 -0300

    alteração

---
+++ for.js
@@ -0,0 +1,14 @@
+const cars = ['BMW', 'Mercedes', 'Toyota', 'Ford', 'Honda', 'Wolksvagen', 'Audi', 'Nissan', 'Kia', 'Hyundai'];
+
+// for(let i = 0; i < 7; i++){ // Estrutura de repetição for
+//     console.log(cars[i]);
+// };
+
+// for (let car of cars){ // Estrutura de repetição for of, ´percorre cada elemento do array e executa o código a cada vez
+//     console.log(car);
+// };
+
+cars.forEach(function(cars, index){ // Estrutura de repetição forEach, ele percorre cada elemento do Array e executa o código, o diferencial é o uso de uma função.. assim possibilitando diversas funcionalidades como por exemplo o uso do index
+    console.log(index, cars);
+    }
+);
