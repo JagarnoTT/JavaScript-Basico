0fe63fefd997c88963c99ec56ccc456c7502727d
Ericles Roque <jagarnotprev@gmail.com>
2025-07-04 21:35:25 -0300

    alteração

---
+++ condicionais.js
@@ -0,0 +1,13 @@
+const sum = 1 + 2;
+if (sum === 3){
+    console.log("Sum is 3");
+}
+else if (sum === 4){
+    console.log("Sum is 4");
+}
+else{
+    console.log("Sum is not 3 or 4");
+};
+
+let sun = sum === 3 ? "true" : "false";
+console.log(sun)
