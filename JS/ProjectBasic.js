5c6f88bfa57184ab9a34ff2f30a17b048314c353
Ericles Roque <jagarnotprev@gmail.com>
2025-07-16 21:21:15 -0300

    alteração

---
+++ JS/ProjectBasic.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const display = document.querySelector('.display');
+    const btnNumbers = document.querySelectorAll('.btnNumber');
+    const btnOperators = document.querySelectorAll('.btnOperator')
+
+    const btnClear = document.querySelector('.btnClear');
+    const btnEqual = document.querySelector('.btnEqual')
+    }
+    
+);
