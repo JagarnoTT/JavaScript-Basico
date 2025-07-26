94fa2a8fe492e92b3d68fd49120ca788d3f0cc62
Ericles Roque <jagarnotprev@gmail.com>
2025-07-26 20:20:49 -0300

    alteração

---
+++ JS/MapFilterReduceEtc/map.js
@@ -0,0 +1,29 @@
+// const number = [1,2,3,4,5,6,7,8];
+
+// const NumberforTwo = number.map(numbers =>
+//  numbers * 2    
+// );
+
+// console.log(NumberforTwo);
+
+// const Ages = [11, 13, 16, 21, 24, 27, 29, 32];
+
+// const length = Ages.length
+// console.log(length)
+
+// const AgesPair = Ages.filter(Agespair => 
+//     Agespair % 2 === 0
+// );
+// console.log(`Os seguintes numeros da lista são par ${AgesPair}!`)
+
+// const AgesImpar =Ages.filter(Agesimpar =>
+//     Agesimpar % 2 !== 0
+// )
+// console.log(`Os seguintes numeros da lista são impar ${AgesImpar}!`)
+
+// const AgesAcumulator = Ages.reduce(function (ages, reduce){
+//     return ages + reduce
+// }, 0)
+// console.log(`A soma das idades é igual a ${AgesAcumulator}!`)
+
+// 1 - find() - Encontra o primeiro item que 
