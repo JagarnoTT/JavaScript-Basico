61b91312913dd13cf120327cefb79ad5161a104b
Ericles Roque <jagarnotprev@gmail.com>
2025-08-24 18:30:02 -0300

    alteração

--- Functions.js
+++ Functions.js
@@ -1,39 +1,73 @@
-const soma = (a, b = 10)=>
- a + b;
-const Soma = soma(2)
-console.log(Soma);
+// const soma = (a, b = 10)=>
+//  a + b;
+// const Soma = soma(2)
+// console.log(Soma);
 
-let cidade = ['paranavai', 'maringa','curitiba', 'blumenau', 'florianópolis'];
+// let cidade = ['paranavai', 'maringa','curitiba', 'blumenau', 'florianópolis'];
 
-// function CadaCidade(indice){
+// // function CadaCidade(indice){
 
-//     indice = indice.toLowerCase();
+// //     indice = indice.toLowerCase();
 
-//     if (cidade.includes(indice)){
-//         return `A cidade ${indice} está na lista`
+// //     if (cidade.includes(indice)){
+// //         return `A cidade ${indice} está na lista`
+// //     }
+// //     else{
+// //         return `A cidade ${indice} não está na lista`
+// //     }
+// // }
+// // console.log(CadaCidade('curitiba'))
+// // console.log(CadaCidade('OSASCO'));
+// // console.log(CadaCidade('blumenau'));
+
+// function CadaCidade(EscolherCidade){
+
+//     EscolherCidade = EscolherCidade.toLowerCase();
+
+//     let Cidadeformatada = EscolherCidade[0].toUpperCase() + EscolherCidade.slice(1).toLowerCase();
+
+//     if(Cidadeformatada.includes(EscolherCidade)){
+//         return `✅ A cidade ${Cidadeformatada} está na lista`
 //     }
 //     else{
-//         return `A cidade ${indice} não está na lista`
+//         return `❌ A cidade ${Cidadeformatada} não está na lista`
 //     }
 // }
-// console.log(CadaCidade('curitiba'))
+// console.log(CadaCidade('PaRaNaVaI'));
+// // console.log(CadaCidade('curiTIBA'));
+// // console.log(CadaCidade('BLUmenau'));
 // console.log(CadaCidade('OSASCO'));
-// console.log(CadaCidade('blumenau'));
+// console.log(quadrado(5)); // Leitura da função
 
-function CadaCidade(EscolherCidade){
+// function quadrado(numero){ // Comando inicial nome + parametros > corpo da função
+// 	return numero * numero}
 
-    EscolherCidade = EscolherCidade.toLowerCase();
 
-    let Cidadeformatada = EscolherCidade[0].toUpperCase() + EscolherCidade.slice(1).toLowerCase();
+// let fator = function fatorial(n){
+//     if(n <= 1){
+//         return 1
+//     }
 
-    if(Cidadeformatada.includes(EscolherCidade)){
-        return `✅ A cidade ${Cidadeformatada} está na lista`
-    }
-    else{
-        return `❌ A cidade ${Cidadeformatada} não está na lista`
-    }
+//     return n * fatorial(n -1)
+// }
+
+// console.log(fator(3));
+
+function meuCarro(carro){
+    carro.marca = "Toyota";
+
 }
-console.log(CadaCidade('PaRaNaVaI'));
-console.log(CadaCidade('curiTIBA'));
-console.log(CadaCidade('BLUmenau'));
-console.log(CadaCidade('OSASCO'));
+
+const meucarro = {marca: 'Honda', modelo: 'Civic'}
+
+meuCarro(meucarro);
+console.log(meucarro);
+console.log(meucarro.marca);
+
+function Compras(item){
+item.proteina = 'carne';
+}
+const itens = {proteina: 'frango', carboidrato: 'arroz'}
+Compras(itens)
+console.log(itens)
+    
