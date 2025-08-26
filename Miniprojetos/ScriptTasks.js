9194aad9030936a9efd68856dc2ae69fcb8bc40b
Ericles Roque <jagarnotprev@gmail.com>
2025-08-25 22:33:18 -0300

    alteração

--- Miniprojetos/ScriptTasks.js
+++ Miniprojetos/ScriptTasks.js
@@ -2,7 +2,7 @@ const input = document.querySelector('.text-task');
 const button = document.querySelector('.button');
 const addTask = document.querySelector('.new-tasks');
 
-const ValidateInput = () =>  input.value.trim().length > 0; //Validando se conteudo do nput é maior que 0;
+const ValidateInput = () =>  input.value.trim().length > 0; //Validando se conteudo do input é maior que 0;
 
 const ValidadeTasks = () =>{ // Função para verificar a tarefa
     const InputisValid = ValidateInput();
@@ -11,33 +11,33 @@ const ValidadeTasks = () =>{ // Função para verificar a tarefa
         return input.classList.add('error')
     }
 
-    const NewTask = document.createElement('div');
-    NewTask.classList.add('ContainerTask');
+    const NewTask = document.createElement('div'); //Criando novo elemento pai
+    NewTask.classList.add('ContainerTask');//Adicionando classe
 
-    const TaskContent = document.createElement('p');
+    const TaskContent = document.createElement('p');//Criando novo elemento filho
     TaskContent.textContent = input.value;
 
     TaskContent.addEventListener('click', () => HandleClick(TaskContent)) 
 
-    const TrashTask = document.createElement('i');
-    TrashTask.classList.add('fa-solid');
-    TrashTask.classList.add('fa-trash-can');
+    const TrashTask = document.createElement('i');//Criando novo elemento filho
+    TrashTask.classList.add('fa-solid');//Adicionando classe
+    TrashTask.classList.add('fa-trash-can');//Adicionando classe
 
-    TrashTask.addEventListener('click', ()=> HandleTaskDelete( NewTask,TaskContent))
+    TrashTask.addEventListener('click', ()=> HandleTaskDelete( NewTask,TaskContent)) //Adicionando função para interação com elemento
 
-    addTask.appendChild(NewTask);
-    NewTask.appendChild(TaskContent);
-    NewTask.appendChild(TrashTask);
+    addTask.appendChild(NewTask); //Declarando nós
+    NewTask.appendChild(TaskContent);//Declarando nós
+    NewTask.appendChild(TrashTask);//Declarando nós
 
-    input.value = '';
+    input.value = ''; //Limpando input inserir tasks
 }
 
-const HandleClick = (TaskContent) =>{
-    const tasks = addTask.children;
+const HandleClick = (TaskContent) =>{ //Adicionando função de interação
+    const tasks = addTask.children; //leitura de nós dentro do elemento pai
 
-    for(const task of tasks){
-        const TaskClicked = task.firstChild.isSameNode(TaskContent)
-        if(TaskClicked)
+    for(const task of tasks){ //loop de leitura dos nós
+        const TaskClicked = task.firstChild.isSameNode(TaskContent) //verificação de conteudo se é o mesmo que está sendo interagido
+        if(TaskClicked) //Condição
             task.firstChild.classList.toggle('Complete')
     }
 }
