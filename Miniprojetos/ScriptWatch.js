b8ed0dfe13f838cf79ae390f443a7d315c561e3e
Ericles Roque <jagarnotprev@gmail.com>
2025-09-28 18:10:45 -0300

    alteração

--- Miniprojetos/ScriptWatch.js
+++ Miniprojetos/ScriptWatch.js
@@ -1,3 +1,6 @@
+let inputCampo = document.querySelector('.input-campo');
+let botao = document.querySelector('.add-Tarefa');
+
 function AtualizarHorario(){
     let Agora = new Date()
 
@@ -15,32 +18,47 @@ function AtualizarHorario(){
 
 let Periodo = Agora.getHours()
 
-if(Periodo >= 7 && Periodo <= 12){
-    document.body.style.background = 'linear-gradient(145deg, #FFE797, #F5EFE6)'
-    document.body.style.color = '#FCB454'
-    
-}
-else if(Periodo >= 13 && Periodo <= 17){
-    document.body.style.background = 'linear-gradient(145deg, #FFC785, #FEFFC4)'
-    document.body.style.color = '#F68537'
-    }
-else if(Periodo >= 18 && Periodo <= 23){
-    document.body.style.background = 'linear-gradient(145deg, #17153B, #4B70F5)'
-    document.body.style.color = '#6EACDA'
-    }
-else{
-    document.body.style.background = 'linear-gradient(145deg, #090040, #471396)'
-    document.body.style.color = '#4E31AA'
-    }
+ if(Periodo >= 7 && Periodo <= 12){
+     document.body.style.background = 'linear-gradient(145deg, #FFE797, #F5EFE6)'
+     document.body.style.color = '#FCB454'
+     inputCampo.style.background = 'linear-gradient(145deg, #f7ac56ff, #FEFFC4)'
+     botao.style.background = '#F68537'
+     botao.style.color = '#ffff'   
+ }
+ else if(Periodo >= 13 && Periodo <= 17){
+     document.body.style.background = 'linear-gradient(145deg, #f7a64bff, #fafaf5ff)'
+     inputCampo.style.background = 'linear-gradient(145deg, #f17c45ff, #f4f5dbff)'
+     document.body.style.color = '#F68537'
+     botao.style.background = '#f34519ff'
+     botao.style.color = '#ffff'
+     }
+ else if(Periodo >= 18 && Periodo <= 23){
+     document.body.style.background = 'linear-gradient(145deg, #17153B, #4B70F5)'
+     inputCampo.style.background = 'linear-gradient(145deg, #4B70F5, #fdfdfbff)'
+     document.body.style.color = '#6EACDA'
+     botao.style.background = '#090040'
+     botao.style.color = '#ffff'
+     }
+ else{
+     document.body.style.background = 'linear-gradient(145deg, #090040, #471396)'
+     document.body.style.color = '#4E31AA'
+     }
 }
 
-setInterval(AtualizarHorario, 1000);
+setInterval(AtualizarHorario,1000)
 
 AtualizarHorario();
 
 const Tarefa = document.querySelector('.Tarefa');
 const botão = document.querySelector('.add-Tarefa');
-const TarefasCampo = document.querySelector('.Tarefas-lista')
+const TarefasCampo = document.querySelector('.Tarefas-lista');
+
+Tarefa.addEventListener('keydown', (event)=>{
+    if(event.key === 'Enter' && ValidarTarefa()) {
+        TarefaInValida();
+        
+    }
+})
 
 const ValidarTarefa = () => Tarefa.value.trim().length > 0;
 
